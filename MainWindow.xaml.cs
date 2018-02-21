547985a07427d4cd4c0c3e9e008c5089e72d6ba4
IanMcT <ian.mctavish@tldsb.on.ca>
2018-02-21 11:37:28 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace stringsDemoProgram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string myFirstString;//Declare a string variable
+        
+        public MainWindow()
+        {
+            myFirstString = "Oh what a beautiful Morning!!!";//Assign a value to the variable
+            
+
+            InitializeComponent();
+            Console.WriteLine(myFirstString);
+            mainWindow.Title = myFirstString;
+        }
+
+        private void btnSetName_Click(object sender, RoutedEventArgs e)
+        {
+            myFirstString = txtFirstName.Text;
+            if (myFirstString == "McT") {
+                MessageBox.Show("So good to see you " + myFirstString);
+            }
+            mainWindow.Title = "Hello " + myFirstString;
+            btnSetName.Content = myFirstString + " change the title";
+            MessageBox.Show(myFirstString.IndexOf("B").ToString());
+
+        }
+    }
+}
+
