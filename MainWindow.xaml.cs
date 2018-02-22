f4bfe137f731cedb9fa848142ada0bf87669dc8e
IanMcT <ian.mctavish@tldsb.on.ca>
2018-02-22 11:51:15 -0500

    fixed substring
    Fixed loop for substring forward section.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -47,7 +47,7 @@ namespace stringsDemoProgram
             string temp = "";
             Console.WriteLine("Length: " + strDog.Length);
             //forwards
-            for (int i = 0; i < strDog.Length; i--)
+            for (int i = 0; i < strDog.Length; i++)
             {
                 temp += strDog.Substring(i, 1) + "\n";
 
